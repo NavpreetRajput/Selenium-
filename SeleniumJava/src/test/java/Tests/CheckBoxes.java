12d447b40fba26193071613a632c9b6af7c24d3c
Navpreet Rajput <navpreet.k.rajput@gmail.com>
2021-06-15 01:20:22 -0400

    Testing of check boxes by checking and unchecking them.

---
+++ SeleniumJava/src/test/java/Tests/CheckBoxes.java
@@ -0,0 +1,69 @@
+package Tests;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+import org.testng.annotations.BeforeMethod;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class CheckBoxes {
+
+	private static WebDriver driver = null;
+	public static String browserName = null;
+
+	@BeforeTest
+	public void setUp() {
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+
+	}
+
+	@Test
+	public void testingAB() {
+
+		// Go to the page of dummy Website
+		driver.get("http://the-internet.herokuapp.com");
+
+		// Click the A/B Testing button
+		driver.findElement(By.xpath("//*[@id=\"content\"]/ul/li[6]/a")).click();
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		driver.findElement(By.xpath("//*[@id=\"checkboxes\"]/input[1]")).click();
+
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		driver.findElement(By.xpath("//*[@id=\"checkboxes\"]/input[2]")).click();
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	@AfterTest
+	public void tearDown() {
+		driver.close();
+		driver.quit();
+		System.out.println("Tested successfully");
+	}
+
+}
