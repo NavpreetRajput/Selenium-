17d3c36c8f7c26d05669a6daf48865638d0ee823
Navpreet Rajput <navpreet.k.rajput@gmail.com>
2021-06-15 12:44:57 -0400

    Changes in Commenting of code

--- SeleniumJava/src/test/java/Tests/CheckBoxes.java
+++ SeleniumJava/src/test/java/Tests/CheckBoxes.java
@@ -32,7 +32,7 @@ public class CheckBoxes {
 		// Go to the page of dummy Website
 		driver.get("http://the-internet.herokuapp.com");
 
-		// Click the A/B Testing button
+		// Click the check boxes link
 		driver.findElement(By.xpath("//*[@id=\"content\"]/ul/li[6]/a")).click();
 		try {
 			Thread.sleep(2000);
@@ -40,6 +40,7 @@ public class CheckBoxes {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+//		Checking the unchecked box
 		driver.findElement(By.xpath("//*[@id=\"checkboxes\"]/input[1]")).click();
 
 		try {
@@ -48,7 +49,7 @@ public class CheckBoxes {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
+//Unchecking the checked box
 		driver.findElement(By.xpath("//*[@id=\"checkboxes\"]/input[2]")).click();
 		try {
 			Thread.sleep(2000);
